3646706252d690a236648ddbcc68a2ecd2a4343f
CM Student <cmstudent@uhwo-acm-10.uhwo.hawaii.edu>
2020-02-20 10:42:56 -1000

    Completed Hackwich#5 Part 1 to Part 8

--- HackWichFive/ViewController.swift
+++ HackWichFive/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var bottomlabel: UILabel!
+    @IBOutlet weak var toplabel: UILabel!
+    @IBOutlet weak var buttonlabel: UIButton!
+    
+    
+    var currentIndex = 0
+    var favoriteFoodArray = ["Pizza", "Hotdogs", "BBQ Chicken", "Pasta", "Hamburger"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.toplabel.text="My Favorite Foods"
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+   
+    //created an ibaction to execute a series of tasks (lines of code) upon button press
+    @IBAction func buttonpressed(_ sender: Any)
+    
+    {
+        //created an bottomlabel to replecae with (favoriteFoodArray) on the text
+        self.bottomlabel.text=favoriteFoodArray[currentIndex]
+        //created currentIndex is less than FavoriteFoodArray
+        if self.currentIndex < self.favoriteFoodArray.count
+        {
+         //created currentIndex plus can equal one
+            currentIndex+=1
+         //created buttonlabel for title (Control is set to default)
+           buttonlabel.setTitle("Next", for: UIControl.State.normal)
+        
+        }
+        else
+        {
+            //(lines of code with button) upon to equal false
+            (sender as! UIButton).isEnabled = false
+        
+        
+        }
+        //compelted part 7
+    
 }
-
+}
